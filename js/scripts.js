433fdc18c76df14ca2d0254c77cdbca4e8ddfd6c
David Jandron <jandron1211@gmail.com>
2022-10-28 11:53:34 -0700

    ran tdd tests in console, added css elements, researched proper looping code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
+const roboger = ["Won't you be my neighbor?", "Boop!", "Beep!"];
+const roboReply = ["3", "2", "1"];
+
+//Business Logic
 
 
 
 
 
-
-
+//User Interface Logic//
 document.ready(function () {
   ("form").submit(function (event) {
       event.preventDefault();
