d57d05a1bca418263419b582f08c57bf81dc8428
David Jandron <jandron1211@gmail.com>
2022-10-28 13:17:42 -0700

    testing counting function and research on mdn site

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,26 @@ const roboger = ["Won't you be my neighbor?", "Boop!", "Beep!"];
 const roboReply = ["3", "2", "1"];
 
 //Business Logic
+function countUp(number) {
+  const countArray = number;
+  let numberArray = [];
+  for (let i = 0; i < countArray; i++); {
+    numberArray.push( i + 1);
+  } return numberArray
+}
 
 
 
 
 
 //User Interface Logic//
+const robo = ["Won't you be my neighbor?", "Boop!", "Beep!"];
+
 document.ready(function () {
   ("form").submit(function (event) {
       event.preventDefault();
-      const userInput = $("#user-input").val();
-        const roboger = roboger(userInput);
+      const userInput = ("#user-input").val();
+        const robo = robo(userInput);
         ("#response").text(reply);
         ("#response").slideDown(reply);
   });
