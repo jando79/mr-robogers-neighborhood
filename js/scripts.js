bb37f6ae63c9016ab6d4d3369bbac852980dffdf
David Jandron <jandron1211@gmail.com>
2023-01-20 13:22:35 -0800

    ADD BI Logic, WIP bugfix

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 //Business Logic
 
-let roboArray1 = [1];
-let robogerResponseArray = [];
-
-For (let index = 0; index >= roboArray1.length; index +=1) {
-robogerResponseArray.push(roboArray1[index] ="Beep!");
-}
-//User Interface Logic
-function handleFormSubmission() {
-  event.preventDefault();
+function countUp(input) {
+  let countUpArray = [];
+  for(let index = input; index >= 0; i --) {
+    countUpArray.push(i);
+  }
+  countUpArray.reverse();
+  return countUpArray;
 }
 
 
