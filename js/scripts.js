4a9697769ab2d8bc7277031d7f283a54b4ebffbd
David Jandron <jandron1211@gmail.com>
2023-01-20 14:29:57 -0800

    BUGFIX in business interface code

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,26 @@
 
 function countUp(input) {
   let countUpArray = [];
-  for(let index = input; index >= 0; i --) {
-    countUpArray.push(i);
+  for(let index = input; index >= 0; index --) {
+    countUpArray.push(index);
   }
   countUpArray.reverse();
   return countUpArray;
 }
 
+function robogerResponds(countUpArray) {
+  
+}
 
+
+
+//User Interface Logic
 function numberInput(event) {
   event.preventDefault();
   const numberInput = document.getElementById("numberInput").value
   let newInput = countUp(numberInput)
 
-  document.getElementById("return").innerText = beepBoop(newInput);
+  document.getElementById("return").innerText = robogerResponds(newInput);
  }
 
  window.addEventListener("load", function() {
