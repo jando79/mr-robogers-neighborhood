a9729f2f471b906be4f649f1f93569760cfccbec
David Jandron <jandron1211@gmail.com>
2023-01-20 11:19:29 -0800

    WIP coding through tests, trouble shooting, research

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 
+function convertToPigLatin(sentence) {
+  let words = sentence.split(" ");
+  let pigLatinWords = words.map(function(word) {
+    let firstLetter = word[0];
+    return word.slice(1) + firstLetter + "ay";
+  });
+  return pigLatinWords.join(" ");
+}
 
+function convertToPigLatin(sentence) {
+  let words = sentence.split(" ");
+  let pigLatinWords = words.map(function(word) {
+    if(/^[aeiou]/i.test(word)) return word+"way"; // start with vowels
+    if(/^qu/i.test(word)) return word.slice(2) + "quay"; // start with qu
+    let firstLetter = word[0];
 
 
 
